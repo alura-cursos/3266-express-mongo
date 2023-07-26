9d9039c783cb85de7a6c88cd6b44f46ce7d88b55
JulianaAmoasei <late.parrot@gmail.com>
2023-07-26 18:01:32 -0300

    video: buscando e atualizando livros

--- src/app.js
+++ src/app.js
@@ -14,6 +14,12 @@ const livros = [
   }
 ]
 
+function buscaLivro(id) {
+  return livros.findIndex(livro => {
+    return livro.id === Number(id);
+  })
+}
+
 app.get("/", (req, res) => {
   res.status(200).send("Curso de Node.js");
 });
@@ -22,9 +28,20 @@ app.get("/livros", (req, res) => {
   res.status(200).json(livros);
 });
 
+app.get("/livros/:id", (req, res) => {
+  const index = buscaLivro(req.params.id);
+  res.status(200).json(livros[index]);
+})
+
 app.post("/livros", (req, res) => {
   livros.push(req.body);
   res.status(201).send("livro cadastrado com sucesso");
 });
+
+app.put("/livros/:id", (req, res) => {
+  const index = buscaLivro(req.params.id);
+  livros[index].titulo = req.body.titulo;
+  res.status(200).json(livros);
+})
 
 export default app;
