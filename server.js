e90949cbc6cd4739b4900656047fcc619c0474f2
JulianaAmoasei <late.parrot@gmail.com>
2023-07-26 16:06:04 -0300

    video: criando rotas

--- server.js
+++ server.js
@@ -2,9 +2,15 @@ import http from "http";
 
 const PORT = 3000;
 
+const rotas = {
+  "/": "Curso de Express API",
+  "/livros": "Entrei na rota livros",
+  "/autores": "Entrei na rota autores"
+};
+
 const server = http.createServer((req, res) => {
   res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end("Curso de Node.js");
+  res.end(rotas[req.url]);
 });
 
 server.listen(PORT, () => {
