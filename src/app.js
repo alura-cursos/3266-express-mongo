728c1848a4fe96522852bf82597b90adecb90d8b
JulianaAmoasei <late.parrot@gmail.com>
2023-07-26 17:24:11 -0300

    video: criando a rota livros

--- src/app.js
+++ src/app.js
@@ -2,8 +2,23 @@ import express from "express";
 
 const app = express();
 
+const livros = [
+  {
+    id: 1,
+    titulo: "O Senhor dos Anéis"
+  },
+  {
+    id: 2,
+    titulo: "O Hobbit"
+  }
+]
+
 app.get("/", (req, res) => {
   res.status(200).send("Curso de Node.js");
+});
+
+app.get("/livros", (req, res) => {
+  res.status(200).json(livros);
 });
 
 export default app;
